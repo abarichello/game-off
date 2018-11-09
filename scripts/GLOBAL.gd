a793dedad06942cba604f64ec82bb286a6e53b64
BARICHELLO <arturbarichello@hotmail.com>
2018-11-09 00:15:13 -0200

    More scaling and resizes

--- scripts/GLOBAL.gd
+++ scripts/GLOBAL.gd
@@ -6,7 +6,7 @@ const HIGHLIGHT = Color(1, 1, 0.25)
 const LOWLIGHT = Color(0.70, 0.30, 1)
 
 const DEF_SPEED: float = 5.0
-const MISSILE_SPEED: Dictionary = { "min": 50, "max": 100 }
+const MISSILE_SPEED: Dictionary = { "min": 30, "max": 60 }
 const BULLET_SPEED: float = 10.0
 
 const LOW_COLLISION: int = 2
